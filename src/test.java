4a211a936a1be7f54e88c0b4dbe67edf06007c99
leila79 <leila.shekofteh@gmail.com>
2022-02-08 09:45:54 +0330

    read XML

--- src/test.java
+++ src/test.java
@@ -13,23 +13,56 @@ public class test {
 
     private static Datacenter[] datacenter;
 
-    private static List<Vm> createVM(int userId, int vms) {
+    enum workflow_type {
+        MONTAGE,
+        CYBER_SHAKE,
+        SIPHT,
+        EPIGENOMICS
+    }
+
+    private static List<Vm> createVM(int userId, workflow_type type) {
         //Creates a container to store VMs. This list is passed to the broker later
         LinkedList<Vm> list = new LinkedList<Vm>();
-
+        int dw = 5;
+        switch (type){
+            case MONTAGE:
+                dw = 8;
+                break;
+            case CYBER_SHAKE:
+                dw = 4;
+                break;
+            case SIPHT:
+                dw = 5;
+                break;
+            case EPIGENOMICS:
+                dw = 7;
+                break;
+        }
         //VM Parameters
         long size = 10000; //image size (MB)
         int ram = 512; //vm memory (MB)
-        int mips = 250;
+        int mips1 = 500;
+        int mips2 = 1000;
+        int mips3 = 1500;
         long bw = 1000;
+        int nw = 500;
+        int vms = (int) Math.ceil(nw / (10 * dw));
         int pesNumber = 1; //number of cpus
         String vmm = "Xen"; //VMM name
 
         //create VMs
-        Vm[] vm = new Vm[vms];
+        Vm[] vm = new Vm[vms * 3];
 
         for (int i = 0; i < vms; i++) {
-            vm[i] = new Vm(datacenter[i].getId(), userId, mips, pesNumber, ram, bw, size, vmm, new CloudletSchedulerSpaceShared());
+            vm[i] = new Vm(datacenter[i].getId(), userId, mips1, pesNumber, ram, bw, size, vmm, new CloudletSchedulerSpaceShared());
+            list.add(vm[i]);
+        }
+        for (int i = vms; i < 2 * vms; i++) {
+            vm[i] = new Vm(datacenter[i].getId(), userId, mips2, pesNumber, ram, bw, size, vmm, new CloudletSchedulerSpaceShared());
+            list.add(vm[i]);
+        }
+        for (int i = 2 * vms; i < 3 * vms; i++) {
+            vm[i] = new Vm(datacenter[i].getId(), userId, mips3, pesNumber, ram, bw, size, vmm, new CloudletSchedulerSpaceShared());
             list.add(vm[i]);
         }
 
@@ -48,7 +81,7 @@ public class test {
             ReplicaCatalog.init(file_system);
             WorkflowParser parser = new WorkflowParser(0, daxPath);
             parser.parse();
-            System.out.println(parser.getTaskList().get(parser.getTaskList().size()-1).toString());
+//            System.out.println(parser.getTaskList().get(parser.getTaskList().size()-1).toString());
 //            System.out.println(parser.getTaskList().size());
 
         } catch (Exception e) {
